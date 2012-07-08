688a5c93bf45c471523490d3188c8e48af37bc58
Ross Hattori <git@exstasis.net>
2012-07-08 13:17:23 -0700

    Adding the dev key and initializer for the Evernote server.

---
+++ config/initializers/evernote_key.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+# This key is for the authentication with the Evernote API,
+# used only for development and testing, should be removed
+# in a production environment.
+AntsV2::Application.config.evernote_dev_key = 'S=s1:U=26f56:E=13f738b8344:C=1381bda5744:P=1cd:A=en-devtoken:H=1fc3322861382db56c9ee0b3ea862e84'
+
+DEV_SERVER = EvernoteServer.new(AntsV2::Application.config.evernote_dev_key)
